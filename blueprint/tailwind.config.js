fe0f16f60451ebb460ac0f36af01a510720564c9
Momanyi Poet <momanyiturner@gmail.com>
2024-04-14 19:52:40 +0300

    all web pages

--- blueprint/tailwind.config.js
+++ blueprint/tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/**/*.{html,js}"],
+  content: [
+    "./build/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppins: ['Poppins'],
+        Shrikhand: ['Shrikhand', 'sans-serif'],
+      },
+      colors: {
+        primary: '#234645',
+        secondary: '#F2D7DB',
+        white: '#FFFAFA',
+        darky: '#262626',
+      },
+    },
   },
   plugins: ['flowbite/plugin'],
 }
